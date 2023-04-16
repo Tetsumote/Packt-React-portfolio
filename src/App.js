4651c4042aff13ea7d0b04b03e661ebd9ffc006c
tetsumote <unionproject88@gmail.com>
2023-04-16 15:49:32 +0200

    end project

--- src/App.js
+++ src/App.js
@@ -3,12 +3,28 @@ import './App.css';
 
 import Header from './components/Header';
 import Profile from './pages/Profile';
+import Projects from './pages/Projects';
+import Project from './pages/ProjectDetail';
+
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Header logo={logo} />
-      <Profile userName="octocat" />
+      <Routes>
+        <Route path='/'
+          element={<Profile userName='octocat' />}
+        />
+        <Route path='/projects'
+          element={<Projects userName='octocat' />}
+        />
+        <Route path='/projects/:name'
+          element={<Project userName='octocat' />}
+        />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
